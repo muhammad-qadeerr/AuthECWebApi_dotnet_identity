72e42651334b920b01a40d13c30d90d9cc34cb5e
Muhammad Qadeer <qadeeransari38@gmail.com>
2025-03-23 01:04:43 +0500

    SignIn Process using JWT and Minimal API

--- AuthECWebApi/Program.cs
+++ AuthECWebApi/Program.cs
@@ -1,7 +1,13 @@
 using AuthECWebApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,6 +35,29 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireLowercase = false;
     options.User.RequireUniqueEmail = true;
 });
+
+// Adding Identity Authtication Service (can be any provider OAuth, JWT etc)    
+builder.Services.AddAuthentication(x =>
+        {
+            // This scheme contains core handlers related to authtication process.
+            x.DefaultAuthenticateScheme =
+            // This scheme contains core handlers related to unauthorized access requests.
+            x.DefaultChallengeScheme =
+
+            x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(y =>
+        {
+            y.SaveToken = true;
+            y.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    builder.Configuration["AppSettings:JWTSecretKey"]!)) // ! vanish null error warnings.
+            };
+        });
+// AddJwtBearer is used to register schemes with necessary configurations.
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -38,6 +67,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// CORS, Authentication, Authorization middle ware must be in same sequence
+#region Config.CORS
+app.UseCors();
+#endregion
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -46,7 +80,7 @@ app.MapControllers();
 app.MapGroup("/api")
    .MapIdentityApi<AppUser>();
 
-// Minimal Post API for tesintg register user endpoint
+// Minimal Post API for  User SignUp
 app.MapPost("/api/signup", async (UserManager<AppUser> userManager,
     [FromBody] UserRegistrationModel userRegistrationModel) =>
 {
@@ -68,6 +102,44 @@ app.MapPost("/api/signup", async (UserManager<AppUser> userManager,
 
 });
 
+
+// Minimal Post API for  User SignIn
+app.MapPost("/api/signin", async (UserManager<AppUser> userManager,
+    [FromBody] UserSignInModel userSignInModel) =>
+{
+    // Validating user
+    var user = await userManager.FindByEmailAsync(userSignInModel.Email);
+    if(user != null && await userManager.CheckPasswordAsync(user, userSignInModel.Password))
+    {
+        // SignIn key for token validation
+        var signInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    builder.Configuration["AppSettings:JWTSecretKey"]!));
+
+        // Token descriptor
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim("UserID", user.Id.ToString())
+            }),
+            //Expires = DateTime.UtcNow.AddMinutes(10),
+            Expires = DateTime.UtcNow.AddDays(10),
+            SigningCredentials = new SigningCredentials(signInKey, SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        // Create a token handler
+        var tokenHandler  = new JwtSecurityTokenHandler();
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+        
+        // Required Desearlized form of token
+        var token = tokenHandler.WriteToken(securityToken);
+        return Results.Ok(new {token});  // Token Structure: Header.Payload.Signature
+
+    }
+    else
+        return Results.BadRequest(new { message = "Email or Password in Incorrect." });
+});
+
 app.Run();
 
 public class UserRegistrationModel
@@ -75,5 +147,11 @@ public class UserRegistrationModel
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string FullName { get; set; } = null!;
+}
+
+public class UserSignInModel
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
 }
 
